dca4f93958387ec811d712b7c6f0e18e4969bdd6
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-05-24 07:45:36 +0300

    finished lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word.lower()
+    def match(self, possible_anagrams):
+        matches=[]
+        sorted_word=sorted(self.word)
+        for candidate in possible_anagrams:
+            if candidate.lower() != self.word and sorted(candidate.lower()) == sorted_word:
+                matches.append(candidate)
+        return matches
